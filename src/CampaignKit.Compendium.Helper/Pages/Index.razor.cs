5882e3faf5f3f3bc040e39582dd78b67049a5ffe
Cory Gill <cory.gill@humber.ca>
2023-09-27 14:24:42 -0400

    Renaming Helper > Index

--- src/CampaignKit.Compendium.Helper/Pages/Index.razor.cs
+++ src/CampaignKit.Compendium.Helper/Pages/Index.razor.cs
@@ -16,6 +16,10 @@
 
 namespace CampaignKit.Compendium.Helper.Pages
 {
+    using CampaignKit.Compendium.Core.Configuration;
+    using CampaignKit.Compendium.Helper.Data;
+    using CampaignKit.Compendium.Helper.Services;
+
     using Microsoft.AspNetCore.Components;
     using Microsoft.JSInterop;
 
@@ -27,39 +31,128 @@ namespace CampaignKit.Compendium.Helper.Pages
     public partial class Index
     {
         /// <summary>
-        /// Gets or sets the ContextMenuService.
+        /// Gets or sets the CompendiumService.
         /// </summary>
         [Inject]
-        protected ContextMenuService ContextMenuService { get; set; }
-
-        /// <summary>
-        /// Gets or sets the DialogService.
-        /// </summary>
-        [Inject]
-        protected DialogService DialogService { get; set; }
+        private CompendiumService CompendiumService { get; set; }
 
         /// <summary>
         /// Gets or sets the JSRuntime for JS interop.
         /// </summary>
         [Inject]
-        protected IJSRuntime JSRuntime { get; set; }
+        private IJSRuntime JSRuntime { get; set; }
 
         /// <summary>
-        /// Gets or sets the NavigationManager.
+        /// Gets or sets the Logger.
         /// </summary>
         [Inject]
-        protected NavigationManager NavigationManager { get; set; }
+        private ILogger<Index> Logger { get; set; }
 
         /// <summary>
-        /// Gets or sets the NotificationService.
+        /// Gets or sets the selected compendium.
         /// </summary>
-        [Inject]
-        protected NotificationService NotificationService { get; set; }
+        private ICompendium SelectedCompendium { get; set; }
 
         /// <summary>
-        /// Gets or sets the TooltipService.
+        /// Gets or sets the selected source data set.
         /// </summary>
-        [Inject]
-        protected TooltipService TooltipService { get; set; }
+        private SourceDataSet SelectedSourceDataSet { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property to store the selected label.
+        /// </summary>
+        private LabelGroup SelectedSourceDataSetGrouping { get; set; }
+
+        /// <summary>
+        /// Handles the compendium collapsed event from the navigator component.
+        /// </summary>
+        /// <param name="compendiumName">The name of the selected compendium.</param>
+        private void OnCompendiumCollapsed(string compendiumName)
+        {
+            this.Logger.LogInformation("SelectedCompendium collapsed: {CompendiumName}", compendiumName);
+
+            // Update user selections
+            this.SelectedSourceDataSetGrouping = null;
+            this.SelectedSourceDataSet = null;
+        }
+
+        /// <summary>
+        /// Handles the compendium expansion event from the navigator component.
+        /// </summary>
+        /// <param name="compendiumName">The name of the selected compendium.</param>
+        private void OnCompendiumExpanded(string compendiumName)
+        {
+            this.Logger.LogInformation("SelectedCompendium expanded: {CompendiumName}", compendiumName);
+
+            // Update user selections
+            this.SelectedSourceDataSetGrouping = null;
+            this.SelectedSourceDataSet = null;
+        }
+
+        /// <summary>
+        /// Logs a message when the label assignment is changed and triggers a state change.
+        /// </summary>
+        private void OnLabelAssignmentChanted(string labelName)
+        {
+            this.Logger.LogInformation("Label assignment changed: {LabelName}", labelName);
+            this.StateHasChanged();
+        }
+
+        /// <summary>
+        /// Handles the label collapse event from the navigator component.
+        /// </summary>
+        /// <param name="labelName">The name of the label that was selected.</param>
+        private void OnLabelCollapsed(string labelName)
+        {
+            this.Logger.LogInformation("Label collapsed: {LabelName}", labelName);
+
+            // Update user selections
+            this.SelectedSourceDataSetGrouping
+                = this.SelectedCompendium.SourceDataSetGroupings.FirstOrDefault(sdsg => sdsg.LabelName.Equals(labelName), null);
+            this.SelectedSourceDataSet = null;
+        }
+
+        /// <summary>
+        /// Handles the label expansion event from the navigator component.
+        /// </summary>
+        /// <param name="labelName">The name of the label that was selected.</param>
+        private void OnLabelExpanded(string labelName)
+        {
+            this.Logger.LogInformation("Label expanded: {LabelName}", labelName);
+
+            // Update user selections
+            this.SelectedSourceDataSetGrouping
+                = this.SelectedCompendium.SourceDataSetGroupings.FirstOrDefault(sdsg => sdsg.LabelName.Equals(labelName), null);
+            this.SelectedSourceDataSet = null;
+        }
+
+        /// <summary>
+        /// Handles the SelectedSource clicked event by logging the selected SelectedSource name.
+        /// </summary>
+        /// <param name="sourceDataSetName">Name of the SelectedSource.</param>
+        private async Task OnSourceDataSetSelected(string sourceDataSetName)
+        {
+            this.Logger.LogInformation("Selected SelectedSource: {SourceDataSetName}", sourceDataSetName);
+
+            // Update user selections
+            this.SelectedSourceDataSetGrouping = null;
+            this.SelectedSourceDataSet
+                = this.SelectedCompendium.SourceDataSets.FirstOrDefault(sds => sds.SourceDataSetName.Equals(sourceDataSetName), null);
+        }
+
+        /// <summary>
+        /// Uploads a compendium and creates a tree from it.
+        /// </summary>
+        /// <param name="args">The upload event arguments.</param>
+        /// <returns>
+        /// A tree created from the uploaded compendium.
+        /// </returns>
+        private async Task UploadComplete(UploadCompleteEventArgs args)
+        {
+            this.Logger.LogInformation("Upload complete and converted to string of length {Length}.", args.RawResponse.Length);
+            var json = args.RawResponse;
+            this.SelectedCompendium = this.CompendiumService.LoadCompendiums(json);
+            this.SelectedSourceDataSet = null;
+        }
     }
 }
